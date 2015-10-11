0cefd3734f3bac35fbe7a902638afb74e6795fbb
Kyle Waters <Kyle@Hera.local>
2015-10-11 22:32:26 +0100

    Commented the Wp-Head

--- public_html/wp-content/themes/bootstrap/header.php
+++ public_html/wp-content/themes/bootstrap/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <?php wp_head(); ?>
+   <?php //wp_head(); ?>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
